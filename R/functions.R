1e9369ab017c12233b5d9512bcb39b0b2691fb7d
Irineo Cabreros <irineo.cabreros@nytimes.com>
2023-05-29 10:49:08 -0400

    first commit

---
+++ R/functions.R
@@ -0,0 +1,51 @@
+add<-function(a,    b){
+
+
+  return_valueForThesum_of_a_and_b_yay <- a+b
+
+
+}
+
+highCyclomatic_complexity <- function(x){
+  if(x == 1){
+    print("yay")
+  } else if(x == 2){
+    print("yay")
+  } else if(x == 3){
+    print("yay")
+  } else if(x == 4){
+    print("yay")
+  } else if(x == 5){
+    print("yay")
+  } else if(x == 6){
+    print("yay")
+  } else if(x == 7){
+    print("x == 2")
+  } else if(x == 8){
+    print("yay")
+  } else if(x == 9){
+    print("yay")
+  } else if(x == 10){
+    print("yay")
+  } else if(x == 11){
+    print("yay")
+  } else if(x == 12){
+    print("yay")
+  } else if(x == 13){
+    print("yay")
+  } else if(x == 14){
+    print("yay")
+  } else if(x == 15){
+    print("yay")
+  } else if(x == 16){
+    print("yay")
+  } else if(x == 17){
+    print("yay")
+  } else if(x == 18){
+    print("yay")
+  } else if(x == 19){
+    print("yay")
+  } else {
+    print("x is big")
+  }
+}
---
+++ random_sketching/analysis1.R
@@ -0,0 +1,19 @@
+temp <- 2
+stooooooooooooooooooooooooooooooooooooooopidThing <- 2*temp
+
+do_Nothing <- function(x){
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return()
+}
